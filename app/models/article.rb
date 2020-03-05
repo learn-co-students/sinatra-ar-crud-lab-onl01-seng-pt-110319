23851e02d47a954271e7b43b8727f80a46e710b5
Nory Lazo <nlazo@me.com>
2020-03-05 18:59:47 -0500

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    # attr_reader :title, :content
+
+    # @@all = []
+
+    # def initalize(params)
+    #     @title = params[:title]
+    #     @content = params[:content]
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+
 end
